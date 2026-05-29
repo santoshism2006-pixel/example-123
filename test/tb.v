b6edc058b8f0cd23dc4a291a0917fb0d63e62c74
santoshism2006-pixel <santoshism2006@gmail.com>
2026-05-29 14:34:22 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,49 +1,35 @@
-`default_nettype none
-`timescale 1ns / 1ps
+import cocotb
+from cocotb.triggers import Timer
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module tb ();
 
-  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.fst");
-    $dumpvars(0, tb);
-    #1;
-  end
+@cocotb.test()
+async def test_4bit_comparator(dut):
 
-  // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+    # Test Case 1 : A > B
+    dut.ui_in.value = 0b00110101   # B=0011 (3), A=0101 (5)
+    await Timer(10, units="ns")
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+    assert dut.uo_out.value & 0b001 == 1, "A > B failed"
+    assert (dut.uo_out.value >> 1) & 1 == 0
+    assert (dut.uo_out.value >> 2) & 1 == 0
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
-  );
+    # Test Case 2 : A == B
+    dut.ui_in.value = 0b01100110   # B=0110 (6), A=0110 (6)
+    await Timer(10, units="ns")
 
-endmodule
+    assert (dut.uo_out.value >> 1) & 1 == 1, "A == B failed"
+    assert dut.uo_out.value & 1 == 0
+    assert (dut.uo_out.value >> 2) & 1 == 0
+
+
+    # Test Case 3 : A < B
+    dut.ui_in.value = 0b01000010   # B=0100 (4), A=0010 (2)
+    await Timer(10, units="ns")
+
+    assert (dut.uo_out.value >> 2) & 1 == 1, "A < B failed"
+    assert dut.uo_out.value & 1 == 0
+    assert (dut.uo_out.value >> 1) & 1 == 0
+
+
+    print("All 4-bit comparator test cases passed!")
